6ff42706833fff341f179a3e6b5b0c420e5eb9ce
Rogerio Almeida Gouvea (rogerio.almeida@uclouvain.be) <rgouvea@mbackf2.cism.ucl.ac.be>
2023-02-08 14:22:33 +0100

    deleted some files from setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MEGNet Tools
-This package makes it easy to manipulate and generate custom MEGNet models. 
+This package can be used to generate and hypertune custom MEGNet models. 
--- build/lib/megnettools/__init__.py
+++
@@ -1,6 +0,0 @@
-#__init__.py
-from .megnet_hyper_tuning import *
-from .megnet_setup_evaluate import *
-from .megnet_featurization import *
-__all__ = megnet_hyper_tuning.__all__ + megnet_setup_evaluate.__all__ + \
-          megnet_featurization.__all__
